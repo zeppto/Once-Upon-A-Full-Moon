45c5b1d38a1ccaa016f5388204afa8d87b568f8d
OSSIAN EDSTRÖM <osed16@student.bth.se>
2019-09-17 11:58:43 +0200

    Initialized and terminated glfw just to make sure it stops complaining.

--- Sandbox/src/Main.cpp
+++ Sandbox/src/Main.cpp
@@ -6,11 +6,16 @@
 int main(int argc, char** argv)
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	if (!glfwInit())
+	{
+		// Initialization failed
+	}
 
 	auto app = new Game();
 	app->Run();
 	delete app;
 
 	system("pause");
+	glfwTerminate();
 	return 0;
 }
