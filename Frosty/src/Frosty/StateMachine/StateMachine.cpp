92e5b1747d9bd1df49d9154eb7f982f27e9514c2
LeOwnYa <leonazohrabian@gmail.com>
2019-09-23 14:27:58 +0200

    State machine added.

---
+++ Frosty/src/Frosty/StateMachine/StateMachine.cpp
@@ -0,0 +1,90 @@
+#include <fypch.hpp>
+#include "StateMachine.hpp"
+
+namespace Frosty
+{
+	StateMachine::~StateMachine()
+	{		
+		Clear();
+	}
+
+	void StateMachine::AddState(StateRef newState, bool isReplacing)
+	{
+		m_IsAdding = true;
+		m_IsReplacing = isReplacing;
+		m_NewState = std::move(newState);
+	}
+	
+	void StateMachine::RemoveState()
+	{
+		m_IsRemoving = true;
+	}
+
+	void StateMachine::ClearAllStates()
+	{
+		m_ClearAll = true;
+	}
+	
+	bool StateMachine::ProcessStateChanges()
+	{
+		// Removing State - Check if stack is empty
+		if (m_IsRemoving && !m_States.empty())
+		{
+			// Not empty - Remove top state
+			m_States.pop();
+			// Check again if stack is empty
+			if (!m_States.empty())
+			{
+				// Not empty - Resume top state
+				m_States.top()->Resume();
+			}
+
+			// Done with removal
+			m_IsRemoving = false;
+			return false;
+		}
+
+		// Check if we want to add a new state
+		if (m_IsAdding)
+		{
+			// Check if stack is empty
+			if (!m_States.empty())
+			{
+				// Not empty - check if we want to replace or not 
+				if (m_IsReplacing)
+				{
+					// Replacing - Remove the top state
+					m_States.pop();
+				}
+				else
+				{
+					// Not replacing - Pause top state
+					m_States.top()->Pause();
+				}
+			}
+
+			// Check to see if we want to clear all states before adding a new one
+			if (m_ClearAll)
+			{
+				Clear();
+			}
+
+			// Add new state on top of the stack and initialize
+			m_IsAdding = false;
+			m_ClearAll = false;
+			m_States.push(std::move(m_NewState));
+			m_States.top()->Initiate();
+			return true;
+		}
+
+		return false;
+	}
+
+	void StateMachine::Clear()
+	{
+		while (!m_States.empty())
+		{
+			m_States.pop();
+		}
+	}
+}
---
+++ Frosty/src/Frosty/StateMachine/StateMachine.hpp
@@ -0,0 +1,40 @@
+#ifndef STATE_MACHINE_HPP
+#define STATE_MACHINE_HPP
+#include "State.hpp"
+
+namespace Frosty
+{
+	typedef std::unique_ptr<State> StateRef;
+
+	class StateMachine
+	{
+	public:
+		StateMachine() { }
+		virtual ~StateMachine();
+		StateMachine(const StateMachine& obj) = delete;
+
+		StateMachine& operator=(const StateMachine& obj) = delete;
+
+		StateRef& GetActiveState() { return m_States.top(); }
+
+		// Replacing means it will pop the current state and add the new one.
+		// Otherwise it will just pause the current state and add another on top of the stack
+		void AddState(StateRef newState, bool isReplacing = true);
+		void RemoveState();
+		void ClearAllStates();
+
+		bool ProcessStateChanges();
+
+	private:
+		void Clear();
+
+		std::stack<StateRef> m_States;
+		StateRef m_NewState;
+
+		bool m_IsRemoving = false;
+		bool m_IsAdding = false;
+		bool m_IsReplacing = false;
+		bool m_ClearAll = false;
+	};
+}
+#endif // !STATE_MACHINE_HPP
