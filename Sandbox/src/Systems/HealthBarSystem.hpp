c4642b451c3d6203f7fde3743e7d1cca5ac9e8cf
Jennifer Larsson-Ståhl <36172747+jenniferls@users.noreply.github.com>
2019-10-30 18:17:17 +0100

    More systems added as files

--- Sandbox/src/Systems/HealthBarSystem.hpp
+++ Sandbox/src/Systems/HealthBarSystem.hpp
@@ -1,1 +1,29 @@
-#pragma once
+#ifndef HEALTH_BAR_SYSTEM_HPP
+#define HEALTH_BAR_SYSTEM_HPP
+#include "Frosty.h"
+#include "Frosty/API/AssetManager.hpp"
+
+class HealthBarSystem : public Frosty::ECS::BaseSystem
+{
+public:
+	HealthBarSystem() = default;
+	virtual ~HealthBarSystem() = default;
+
+	virtual void Init() override;
+
+	virtual void OnUpdate() override;
+
+	virtual void Render() override;
+
+	virtual void AddComponent(const std::shared_ptr<Frosty::ECS::Entity>& entity) override;
+
+	virtual void RemoveEntity(const std::shared_ptr<Frosty::ECS::Entity>& entity) override;
+
+private:
+	std::array<Frosty::ECS::CTransform*, Frosty::ECS::MAX_ENTITIES_PER_COMPONENT> m_Transform;
+	std::array<Frosty::ECS::CHealthBar*, Frosty::ECS::MAX_ENTITIES_PER_COMPONENT> m_HealthBar;
+	std::array<Frosty::ECS::CHealth*, Frosty::ECS::MAX_ENTITIES_PER_COMPONENT> m_Health;
+
+};
+
+#endif // !HEALTH_BAR_SYSTEM_HPP
--- Sandbox/src/Systems/LightSystem.hpp
+++ Sandbox/src/Systems/LightSystem.hpp
@@ -1,1 +1,26 @@
-#pragma once
+#ifndef LIGHT_SYSTEM_HPP
+#define LIGHT_SYSTEM_HPP
+#include "Frosty.h"
+
+class LightSystem : public Frosty::ECS::BaseSystem
+{
+public:
+	LightSystem() = default;
+	virtual ~LightSystem() = default;
+
+	virtual void Init() override;
+
+	virtual void OnUpdate() override;
+
+	virtual void Render() override;
+
+	virtual void AddComponent(const std::shared_ptr<Frosty::ECS::Entity>& entity) override;
+
+	virtual void RemoveEntity(const std::shared_ptr<Frosty::ECS::Entity>& entity) override;
+
+private:
+	std::array<Frosty::ECS::CTransform*, Frosty::ECS::MAX_ENTITIES_PER_COMPONENT> m_Transform;
+	std::array<Frosty::ECS::CLight*, Frosty::ECS::MAX_ENTITIES_PER_COMPONENT> m_Light;
+};
+
+#endif // !LIGHT_SYSTEM_HPP
