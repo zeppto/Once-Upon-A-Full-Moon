1e7f445169a812c6717f9ff9db4520373e6971eb
zeppto <wera.ea97@gmail.com>
2019-11-11 09:08:27 +0100

    fileformat update

--- Sandbox/src/LevelHandeler/LevelFileFormat.cpp
+++ Sandbox/src/LevelHandeler/LevelFileFormat.cpp
@@ -39,9 +39,74 @@ void LevelFileFormat::AddEntity(const std::shared_ptr<Frosty::ECS::Entity>& enti
 		if (m_World->HasComponent<Frosty::ECS::CMesh>(entity))
 		{
 			myComponents.MyComponents.at(1).HaveComponent = true;
+			myComponents.myMesh.MeshName =
 		}
 		else
 			myComponents.MyComponents.at(1).HaveComponent = false;
+		//Material
+		if (m_World->HasComponent<Frosty::ECS::CMaterial>(entity))
+		{
+			myComponents.MyComponents.at(2).HaveComponent = true;
+		}
+		else
+			myComponents.MyComponents.at(2).HaveComponent = false;
+		//Follow
+		if (m_World->HasComponent<Frosty::ECS::CFollow>(entity))
+		{
+			myComponents.MyComponents.at(3).HaveComponent = true;
+		}
+		else
+			myComponents.MyComponents.at(3).HaveComponent = false;
+		//Light
+		if (m_World->HasComponent<Frosty::ECS::CLight>(entity))
+		{
+			myComponents.MyComponents.at(4).HaveComponent = true;
+		}
+		else
+			myComponents.MyComponents.at(4).HaveComponent = false;
+		//Physics
+		if (m_World->HasComponent<Frosty::ECS::CPhysics>(entity))
+		{
+			myComponents.MyComponents.at(5).HaveComponent = true;
+		}
+		else
+			myComponents.MyComponents.at(5).HaveComponent = false;
+		//Enemy
+		if (m_World->HasComponent<Frosty::ECS::CEnemy>(entity))
+		{
+			myComponents.MyComponents.at(6).HaveComponent = true;
+		}
+		else
+			myComponents.MyComponents.at(6).HaveComponent = false;
+		//Health
+		if (m_World->HasComponent<Frosty::ECS::CHealth>(entity))
+		{
+			myComponents.MyComponents.at(7).HaveComponent = true;
+		}
+		else
+			myComponents.MyComponents.at(7).HaveComponent = false;
+		//HealthBar
+		if (m_World->HasComponent<Frosty::ECS::CHealthBar>(entity))
+		{
+			myComponents.MyComponents.at(8).HaveComponent = true;
+		}
+		else
+			myComponents.MyComponents.at(8).HaveComponent = false;
+		//ParticleSystem
+		if (m_World->HasComponent<Frosty::ECS::CParticleSystem>(entity))
+		{
+			myComponents.MyComponents.at(9).HaveComponent = true;
+		}
+		else
+			myComponents.MyComponents.at(9).HaveComponent = false;
+		//LevelExit
+		if (m_World->HasComponent<Frosty::ECS::CLevelExit>(entity))
+		{
+			myComponents.MyComponents.at(10).HaveComponent = true;
+		}
+		else
+			myComponents.MyComponents.at(10).HaveComponent = false;
+
 		m_Entitys.myEntitys.push_back(myComponents);
 	}
 }
