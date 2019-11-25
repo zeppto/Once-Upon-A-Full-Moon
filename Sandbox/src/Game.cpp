0c718d1c5d0c4cc83eb72a1f8f7962f431f1d59e
Jennifer Larsson-Ståhl <36172747+jenniferls@users.noreply.github.com>
2019-11-25 19:22:27 +0100

    Blendmap example in game.cpp
    (acidentally on master branch, have to carefully transfer all code)

--- Sandbox/src/Game.cpp
+++ Sandbox/src/Game.cpp
@@ -366,6 +366,16 @@ namespace MCS
 		//particleSystemComp2.StartParticleSize = 5.0f;
 		//particleSystemComp2.EndParticleSize = 2.0f;
 
+		//Blendmap working
+		//auto& blendTest = world->CreateEntity({ 0.0f, 1.0f, 0.0f }, { 0.0f, 0.0f, 0.0f }, {300.0f, 1.0f, 300.0f});
+		//world->AddComponent<Frosty::ECS::CMesh>(blendTest, Frosty::AssetManager::GetMesh("pPlane1"));
+		//auto& blendShaderComp = world->AddComponent<Frosty::ECS::CMaterial>(blendTest, Frosty::AssetManager::GetShader("BlendShader"));
+		//blendShaderComp.BlendMapTexture = Frosty::AssetManager::GetTexture2D("blend_road_cross");
+		//blendShaderComp.DiffuseTexture = Frosty::AssetManager::GetTexture2D("ground_test2"); //Alpha channel
+		//blendShaderComp.BlendTexture1 = Frosty::AssetManager::GetTexture2D("brown_mud_diffuse"); //Red channel
+		//blendShaderComp.BlendTexture2 = blendShaderComp.DiffuseTexture; //Green channel
+		//blendShaderComp.NormalTexture = Frosty::AssetManager::GetTexture2D("ground_test_normal");
+
 //ifdef FY_DEBUG
 	PushLayer(FY_NEW InspectorLayer());
 //else
