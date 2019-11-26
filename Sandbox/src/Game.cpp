b1380bfde44939254c578116d4b98b7f14f00af2
Jennifer Larsson-Ståhl <36172747+jenniferls@users.noreply.github.com>
2019-11-26 19:04:53 +0100

    Quick comment

--- Sandbox/src/Game.cpp
+++ Sandbox/src/Game.cpp
@@ -64,14 +64,14 @@ namespace MCS
 		particleSystem->AttachGameCamera(&world->GetComponent<Frosty::ECS::CTransform>(world->GetSceneCamera()));
 
 		//Blendmap working
-		auto& blendTest = world->CreateEntity({ 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f }, { 300.0f, 1.0f, 300.0f });
-		world->AddComponent<Frosty::ECS::CMesh>(blendTest, Frosty::AssetManager::GetMesh("pPlane1"));
-		auto& blendShaderComp = world->AddComponent<Frosty::ECS::CMaterial>(blendTest, Frosty::AssetManager::GetShader("BlendShader"));
-		blendShaderComp.BlendMapTexture = Frosty::AssetManager::GetTexture2D("blend_road_cross");
-		blendShaderComp.DiffuseTexture = Frosty::AssetManager::GetTexture2D("ground_test2");
-		blendShaderComp.BlendTexture1 = Frosty::AssetManager::GetTexture2D("road_test2"); //Red channel
-		blendShaderComp.BlendTexture2 = blendShaderComp.DiffuseTexture; //Green channel
-		blendShaderComp.NormalTexture = Frosty::AssetManager::GetTexture2D("ground_test_normal");
+		//auto& blendTest = world->CreateEntity({ 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f }, { 300.0f, 1.0f, 300.0f });
+		//world->AddComponent<Frosty::ECS::CMesh>(blendTest, Frosty::AssetManager::GetMesh("pPlane1"));
+		//auto& blendShaderComp = world->AddComponent<Frosty::ECS::CMaterial>(blendTest, Frosty::AssetManager::GetShader("BlendShader"));
+		//blendShaderComp.BlendMapTexture = Frosty::AssetManager::GetTexture2D("blend_road_cross");
+		//blendShaderComp.DiffuseTexture = Frosty::AssetManager::GetTexture2D("ground_test2");
+		//blendShaderComp.BlendTexture1 = Frosty::AssetManager::GetTexture2D("road_test2"); //Red channel
+		//blendShaderComp.BlendTexture2 = blendShaderComp.DiffuseTexture; //Green channel
+		//blendShaderComp.NormalTexture = Frosty::AssetManager::GetTexture2D("ground_test_normal");
 
 		// SCENE 1
 		// PLANE
